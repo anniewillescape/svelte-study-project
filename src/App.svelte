cc09496357f54109430864fb70ea59f4f55f0c32
anniewillescape <meteor.voice@gmail.com>
2022-05-22 17:24:16 +0900

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Greeting from "./Greeting.svelte";
+  import Card from "./Card.svelte";
+
+  let name = "";
+</script>
+
+<main>
+  <input placeholder="Enter your name" bind:value={name} />
+  <div>Hello, {name}</div>
+</main>
+
+<style></style>
